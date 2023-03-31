b8a93fc1df3c85b1086da478d6ed8c5bb9597996
Jim Lim <ylim0006@student.monash.edu>
2023-03-31 15:59:34 +0800

    first commit

---
+++ tests/test_lottery_unit.py
@@ -0,0 +1,89 @@
+# test with $50 = 0.029 ETH
+
+from brownie import Lottery, accounts, network, config, exceptions
+from scripts.deploy_lottery import deploy_lottery
+from scripts.helpful_scripts import (
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    get_account,
+    fund_with_link,
+)
+from web3 import Web3
+import pytest
+
+
+# def test_get_entrance_fee():
+#     account = accounts[0]
+#     lottery = Lottery.deploy(
+#         config["networks"][network.show_active()]["eth_usd_price_feed"],
+#         {"from": account},
+#     )
+#     # assert lottery.getEntranceFee() > Web3.toWei(0.028, "ether")
+#     # assert lottery.getEntranceFee() < Web3.toWei(0.032, "ether")
+
+
+def test_get_entrance_fee():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    # Arrange
+    lottery = deploy_lottery()
+    # Act
+    # ETH/USD = $1.7k
+    # usdEntryFee = $50
+    expected_entrance_fee = Web3.toWei(0.025, "ether")
+    entrance_fee = lottery.getEntranceFee()
+    # Assert
+    assert expected_entrance_fee == entrance_fee
+
+
+def test_cant_enter_unless_started():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    # Act / Assert
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
+
+
+def test_can_start_and_enter_lottery():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    # Act
+    lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
+    # Assert
+    assert lottery.players(0) == account
+
+
+def test_can_end_lottery():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    # Act
+    lottery.endLottery({"from": account})
+    # Assert
+    assert lottery.lottery_state() == 2  # 2 is CALCULATING_WINNER
+
+
+def test_can_pick_winner_correctly():
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=1), "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=2), "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    # Act
+    # Assert
